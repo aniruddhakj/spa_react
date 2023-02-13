22fddc7aa3f88ff45b350366640d7c01df269ef0
aniruddhakj <aniruddhakj100@gmail.com>
2023-02-13 19:56:57 +0530

    images rendered on user dashboard

--- spa/src/Components/UserDashboard.jsx
+++ spa/src/Components/UserDashboard.jsx
@@ -35,10 +35,15 @@ class UserDashboard extends Component {
 	// add the images to the images array from the src/images folder
 	// there is no label assigned to the images when the page loads
 	componentDidMount() {
-		const images = this.importAll(require.context('/assets/images', false, /\.(jpg)$/));
+		const images = this.importAll(require.context('../../src/images', false, /\.(jpg)$/));
 		// add the images to the images array from the src/images folder
 		// set state in response to the images array
-		this.setState({ images: Object.keys(images).map((image) => ({ id: image, url: images[image].default, label: null })) });
+		// in the url property, set the path to the image
+		this.setState({
+			images: Object.keys(images).map((image) => {
+				return { id: image, url: images[image], label: null };
+			})
+		})
 	}
 
 
@@ -47,13 +52,14 @@ class UserDashboard extends Component {
 		// get the images from the state
 		const { images } = this.state;
 		// check if the images array is not empty
+
 		if (images.length > 0) {
 			// display the images on the dashboard page
 			// fix the image size to 200px
 			return images.map((image) => {
 				return (
 					<div className="image" key={image.id}>
-						<img src={image.url} alt={image.id} />
+						<img src={image.url} alt={image.id} width="200" />
 					</div>
 				);
 			});
@@ -71,66 +77,45 @@ class UserDashboard extends Component {
 		this.setState({ selectedLabel: label });
 	};
 
-	// add a new method to handle the label assignment
-	handleLabelAssignment = () => {
-		const { selectedImage, selectedLabel, images } = this.state;
-		// check if the selected image and label are not null
-		if (selectedImage && selectedLabel) {
-			// find the selected image in the images array
-			const image = images.find((image) => image.id === selectedImage.id);
-			// check if the image is not null
-			if (image) {
-				// check if the image already has a label
-				if (image.label) {
-					// check if the image has the same label as the selected label
-					if (image.label.id === selectedLabel.id) {
-						// remove the label from the image
-						image.label = null;
-					} else {
-						// assign the selected label to the image
-						image.label = selectedLabel;
-					}
-				} else {
-					// assign the selected label to the image
-					image.label = selectedLabel;
-				}
-				// update the images array
-				this.setState({ images });
-			}
+	// add a new method to handle the label addition
+	// if the label is not already present in the labels array, add it to the labels array
+	handleLabelAddition = (label) => {
+		const { labels } = this.state;
+		if (labels.indexOf(label) === -1) {
+			this.setState({ labels: [...labels, label] });
 		}
 	};
 
 	// add a new method to handle the label deletion
-	handleLabelDeletion = () => {
-		const { selectedImage, images } = this.state;
-		// check if the selected image is not null
-		if (selectedImage) {
-			// find the selected image in the images array
-			const image = images.find((image) => image.id === selectedImage.id);
-			// check if the image is not null
-			if (image) {
-				// remove the label from the image
-				image.label = null;
-				// update the images array
-				this.setState({ images });
-			}
+	// if the label is present in the labels array, delete it from the labels array
+	// remove the label from all the images
+
+	handleLabelDeletion = (label) => {
+		const { labels, images } = this.state;
+		if (labels.indexOf(label) !== -1) {
+			this.setState({ labels: labels.filter((l) => l !== label) });
+			this.setState({ images: images.map((image) => ({ ...image, label: image.label === label ? null : image.label })) });
 		}
 	};
 
-	// add a new method to handle the label filtering
-	handleLabelFiltering = (label) => {
-		const { images } = this.state;
-		// check if the label is not null
-		if (label) {
-			// filter the images by the label
-			const filteredImages = images.filter((image) => image.label && image.label.id === label.id);
-			// update the filtered images array
-			this.setState({ filteredImages });
-		} else {
-			// update the filtered images array
-			this.setState({ filteredImages: [] });
+	// add a new method to handle the label assignment
+	// if the label is present in the labels array, assign it to the selected image
+	handleLabelAssignment = (label) => {
+		const { labels, images, selectedImage } = this.state;
+		if (labels.indexOf(label) !== -1) {
+			this.setState({ images: images.map((image) => ({ ...image, label: image.id === selectedImage.id ? label : image.label })) });
 		}
 	};
+
+	// add a new method to handle the label reassignment
+	// if the label is present in the labels array, reassign it to the selected image
+	handleLabelReassignment = (label) => {
+		const { labels, images, selectedImage } = this.state;
+		if (labels.indexOf(label) !== -1) {
+			this.setState({ images: images.map((image) => ({ ...image, label: image.id === selectedImage.id ? label : image.label })) });
+		}
+	};
+
 
 	handleLogout = () => {
 		localStorage.removeItem('user');
@@ -143,94 +128,28 @@ class UserDashboard extends Component {
 			return <Navigate to="/" />;
 		}
 		// display the dashboard page with all the images and labels below each image by default (no label filtering)
-		// give user the option to filter the images by labels
+		// give user the option to filter the images by labels present in the labels array
 		// give user the option to assign, reassign and delete a label on an image
 		// give user the option to logout
 		return (
-			<>
-				{this.state.logout && <Navigate to="/" />}
-				<div className="dashboard">
-					<div className="dashboard-header">
-						<div className="dashboard-header__title">Dashboard</div>
-						<button className="dashboard-header__logout" onClick={this.handleLogout}>
-							Logout
-						</button>
-					</div>
-					<div className="dashboard-content">
-						<div className="dashboard-content__images">{this.displayImages()}</div>
-						<div className="dashboard-content__labels">
-							<div className="dashboard-content__labels-header">
-								<div className="dashboard-content__labels-header-title">Labels</div>
-								<button className="dashboard-content__labels-header-add">Add</button>
+			<div className="dashboard">
+				<div className="images">{this.displayImages()}</div>
+				<div className="labels">
+					{this.state.labels.map((label) => {
+						return (
+							<div className="label" key={label}>
+								{label}
+								<button onClick={() => this.handleLabelAssignment(label)}>Assign</button>
+								<button onClick={() => this.handleLabelReassignment(label)}>Reassign</button>
+								<button onClick={() => this.handleLabelDeletion(label)}>Delete</button>
 							</div>
-							<div className="dashboard-content__labels-list">
-								<div
-									className="dashboard-content__labels-list-item"
-									onClick={() => this.handleLabelFiltering(null)}
-								>
-									All
-								</div>
-								<div
-									className="dashboard-content__labels-list-item"
-									onClick={() => this.handleLabelFiltering({ id: 1, name: 'Label 1' })}
-								>
-									Label 1
-								</div>
-								<div
-									className="dashboard-content__labels-list-item"
-									onClick={() => this.handleLabelFiltering({ id: 2, name: 'Label 2' })}
-								>
-									Label 2
-								</div>
-								<div
-									className="dashboard-content__labels-list-item"
-									onClick={() => this.handleLabelFiltering({ id: 3, name: 'Label 3' })}
-								>
-									Label 3
-								</div>
-							</div>
-						</div>
-					</div>
+						);
+					})}
 				</div>
-				{this.state.selectedImage && (
-					<div className="image-modal">
-						<div className="image-modal__content">
-							<div className="image-modal__content-header">
-								<div className="image-modal__content-header-title">Image</div>
-								<button
-									className="image-modal__content-header-close"
-									onClick={() => this.setState({ selectedImage: null })}
-								>
-									X
-								</button>
-							</div>
-							<div className="image-modal__content-body">
-								<img src={this.state.selectedImage.url} alt={this.state.selectedImage.name || 'No Name'} />
-								<div className="image-modal__content-body-label">
-									{this.state.selectedImage.label ? this.state.selectedImage.label.name : 'No Label'}
-								</div>
-							</div>
-							<div className="image-modal__content-footer">
-								<button
-									className="image-modal__content-footer-assign"
-									onClick={() => this.handleLabelAssignment()}
-								>
-									{this.state.selectedImage.label ? 'Reassign' : 'Assign'}
-								</button>
-								<button
-									className="image-modal__content-footer-delete"
-									onClick={() => this.handleLabelDeletion()}
-								>
-									Delete
-								</button>
-							</div>
-						</div>
-					</div>
-				)}
-			</>
+				<button onClick={this.handleLogout}>Logout</button>
+			</div>
 		);
 	}
-
 }
 
 export default UserDashboard;
